8037fd01c1bd56562247fd33b67e68c1f303aa52
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-05-21 10:39:49 +0200

    feat: create comment routes

---
+++ routes/comment.routes.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const router = require('express').Router();
+const { isAuthenticated } = require('../middleware/jwt.middleware');
+const Comment = require('../models/Comment.model');
+
+router.post('/events/:eventId/comments', isAuthenticated, (req, res, next) => {
+  const { eventId } = req.params;
+  const comment = req.body;
+  const newComment = {
+    ...comment,
+    event: eventId,
+    createdBy: req.payload._id,
+  };
+
+  if (!mongoose.Types.ObjectId.isValid(eventId)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Comment.create(newComment)
+    .then((response) => res.json(response))
+    .catch((err) => res.json(err));
+});
+
+router.get('/events/:eventId/comments', (req, res, next) => {
+  const { eventId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(eventId)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Comment.find({ event: eventId })
+    .then((comments) => res.json(comments))
+    .catch((err) => res.json(err));
+});
+
+module.exports = router;
